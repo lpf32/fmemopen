226c262692158012b9b27f79aa148fcabf0dc8b9
zhangpanpan <zhpan188@gmail.com>
2016-05-13 14:47:12 +0800

    initial commit

---
+++ fmemopen.h
@@ -0,0 +1,13 @@
+//
+// Created by 张盼盼 on 16/5/13.
+//
+
+#ifndef APUE_M_FMEMOPEN_H
+#define APUE_M_FMEMOPEN_H
+
+
+
+FILE *fmemopen(void *buf, size_t size, const char *type);
+
+
+#endif //APUE_M_FMEMOPEN_H
